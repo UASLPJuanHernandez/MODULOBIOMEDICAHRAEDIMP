9085de204a69f6f158498aa9d38a7cbaca182643
nformatica <nformatica@hospital.gob.mx>
2026-01-12 09:34:55 -0600

    Actualizar archivos de configuración y vistas
    - Actualizar archivos artisan y sail
    - Corregir nombres de archivos con caracteres especiales
    - Actualizar .gitignore de storage/framework
    - Agregar archivos de vistas y recursos

---
+++ app/Filament/Resources/Cd/home/vizl24/inventarioHospitalario&&./vendor/bin/sailArtisanOptimize:clearResource/Pages/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\Cd\home\vizl24\inventarioHospitalario&&.\vendor\bin\sailArtisanOptimize:clearResource\Pages;
+
+use App\Filament\Resources\Cd\home\vizl24\inventarioHospitalario&&.\vendor\bin\sailArtisanOptimize:clearResource;
+use Filament\Resources\Pages\Page;
+
+class Dashboard extends Page
+{
+    protected static string $resource = sailArtisanOptimize:clearResource::class;
+
+    protected static string $view = 'filament.resources.cd.home.vizl24.inventario-hospitalario&&..vendor.bin.sail-artisan-optimize:clear-resource.pages.dashboard';
+}
